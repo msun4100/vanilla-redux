5769260bc4f70b1dafbc86afd234be964a45f49b
msun4100 <msun4100@gmail.com>
2020-03-22 03:52:57 +0900

    Use Redux-Toolkit, createSlice

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,22 +1,18 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { actionCreators } from "../store";
-import Todo from "../components/ToDo";
+import { add } from "../store";
+import ToDo from "../components/ToDo";
 
-// use react-hook
 function Home({ toDos, addToDo }) {
-  // console.log(rest);
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
-
   function onSubmit(e) {
     e.preventDefault();
     addToDo(text);
     setText("");
   }
-
   return (
     <>
       <h1>To Do</h1>
@@ -25,8 +21,8 @@ function Home({ toDos, addToDo }) {
         <button>Add</button>
       </form>
       <ul>
-        {toDos.map(todo => (
-          <Todo {...todo} key={todo.id} />
+        {toDos.map(toDo => (
+          <ToDo {...toDo} key={toDo.id} />
         ))}
       </ul>
     </>
@@ -36,11 +32,11 @@ function Home({ toDos, addToDo }) {
 function mapStateToProps(state) {
   return { toDos: state };
 }
+
 function mapDispatchToProps(dispatch) {
   return {
-    addToDo: text => dispatch(actionCreators.addToDo(text))
+    addToDo: text => dispatch(add(text))
   };
 }
 
-// hook과 비슷하게 mapState/Dispatch + connect 하는 부분은 공식처럼 이해
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/store.js
+++ src/store.js
@@ -1,37 +1,19 @@
-import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-/* const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter(todo => todo.id !== action.payload);
-    default:
-      return state;
+// reducers 주의
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) => state.filter(todo => todo.id !== action.payload)
   }
-}; */
-
-// toolkit을 쓰면 mutate를 허용, toolkit이 기존 덮어쓰기? 기능을 대신해줌
-// state object를 mutate 해주거나, or new state obj를 리턴하면 됨
-// === addToDo는 mutation, deleteToDo는 새로운 State 리턴
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
-  },
-  [deleteToDo]: (state, action) =>
-    state.filter(todo => todo.id !== action.payload)
 });
 
-const store = configureStore({ reducer });
+const store = configureStore({ reducer: toDos.reducer });
 
-// store.subscribe() //store 변동사항을 sub 하기 위해 react-redux가 필요
-
-export const actionCreators = {
-  addToDo,
-  deleteToDo
-};
+export const { add, remove } = toDos.actions;
 
 export default store;
